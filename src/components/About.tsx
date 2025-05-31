df77cdd4b45bb8fff30b63a09d67b692494e5af7
Danny <ubahchuks91@gmail.com>
2025-05-31 12:08:46 +0100

    implement hero section

---
+++ src/components/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type AboutProps = {}
+
+const About: React.FC<AboutProps> = (props) => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,3 +1,4 @@
+import { ArrowDown } from 'lucide-react';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -35,6 +36,11 @@ const HeroSection: React.FC<HeroSectionProps> = props => {
 						<Link to='#projects' className='cosmic-button'>View My Work</Link>
 					</div>
 				</div>
+			</div>
+
+			<div className='absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center justify-center animate-bounce'>
+				<span className='text-sm text-muted-foreground mb-2'>Scroll</span>
+				<ArrowDown className='h-5 w-5 text-primary' />
 			</div>
 		</section>
 	);
